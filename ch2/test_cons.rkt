c079ad300a77357226a0878291a23d52da53f03a
sunsai <sunsai.cs@gmail.com>
2015-03-06 14:54:42 +0800

    chp2 cons test

---
+++ ch2/test_cons.rkt
@@ -0,0 +1,49 @@
+#lang slideshow
+
+(define x (cons 1 2))
+
+(define y (cons 3 4))
+
+(define z (cons x y))
+
+(define (add-rat x y)
+  (make-rat (+ (* (numer x)
+              (denom y))
+           (* (numer y)
+              (denom x)))
+        (* (denom x)(denom y))))
+
+(define (sub-rat x y)
+  (make-rat (- (* (numer x)
+                  (denom y))
+               (* (numer y)
+                  (denom x)))
+            (* (denom x)
+               (denom y))))
+
+(define (mul-rat x y)
+  (make-rat (* (numer x)
+               (numer y))
+            (* (denom x)
+               (denom y))))
+
+(define (div-rat x y)
+  (make-rat (* (numer x)
+               (denom y))
+            (* (numer y)
+               (denom y))))
+
+(define (equal-rat x y)
+  (= (* (numer x)
+        (denom y))
+     (* (numer y)
+        (numer x))))
+               
+(define (make-rat n d)
+  (cons n d))
+
+(define (numer x)
+  (car x))
+
+(define (denom x)
+  (cdr x))
