990b0eddd91205ea16c6ef9aecea947712f45a67
sunsai <sunsai.cs@gmail.com>
2015-03-07 18:41:27 +0800

    count-leaves

---
+++ ch2/count-leaves.rkt
@@ -0,0 +1,9 @@
+#lang racket
+
+(define (count-leaves x)
+  (cond ((null? x) 0)
+        ((not (pair? x)) 1)
+        (else (+ (count-leaves (car x))
+                 (count-leaves (cdr x))))))
+
+(define x (cons (cons 1 2) (cons 3 4)))
